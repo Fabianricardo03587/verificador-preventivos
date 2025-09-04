cf0b451459933242a9ee20f2788969681c48a8dd
Fabianricardo03587 <fabianricardo03587@gmail.com>
2025-09-04 17:37:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ else:
       st.markdown("""
         <style>
         .block-container {
-            max-width: 60%;
+            max-width: 50%;
             padding-top: 1rem;
         }
         h1, h2, h3 {
@@ -268,6 +268,7 @@ if st.session_state.autenticado:
     if st.button("Cerrar sesión"):
         st.session_state.autenticado = False
         st.experimental_rerun()
+
 
 
 
