469d7d9b12f358c64799f4fce72790c62582fbf9
Fabianricardo03587 <fabianricardo03587@gmail.com>
2025-09-05 12:45:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ if not st.session_state.autenticado:
     .stButton > button {
         display: block;       /* importante: que sea un bloque */
         margin: 0 auto;   /* centra horizontalmente */
-        width: 50%;           /* Usa el 80% del contenedor padre */
+        width: 100%;           /* Usa el 80% del contenedor padre */
         max-width: 300px;     /* Limita el ancho máximo para que no se vea gigante */
         padding: 10px 40px;      /* Más espacio vertical, sin mucho horizontal */
         background-color: #2542FF;
@@ -353,6 +353,7 @@ if st.session_state.autenticado:
     if st.button("Cerrar sesión"):
         st.session_state.autenticado = False
         st.experimental_rerun()
+
 
 
 
